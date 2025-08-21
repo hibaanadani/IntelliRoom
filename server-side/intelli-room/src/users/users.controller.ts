615099d86650220dcd70bc1ad23cb783cb44b1d8
hibaanadani <hibaanadani1@gmail.com>
2025-08-21 21:24:02 +0300

    started working on the users controller

--- server-side/intelli-room/src/users/users.controller.ts
+++ server-side/intelli-room/src/users/users.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    // nest js utilizing dependancy injection and instantiate classes and manage referenc to those classes
+    constructor(private userService: UsersService){}
+
+    @Get()
+    getUser(): any{
+        return [{id:0}];
+    }
+
+    // : marks the id as dynamic value URL parameter
+    // parse the id from the URL and provide it to the service
+    // param 1 to many, specify id and giv eit name id and type string
+    @Get(':id')
+    getUserById(@Param('id') id: string): any{
+        return {
+            id
+        }
+    }
+}
