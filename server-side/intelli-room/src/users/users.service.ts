4699da4ac7dd02d8c01e947522db1ba05049a2d6
hibaanadani <hibaanadani1@gmail.com>
2025-08-21 21:44:46 +0300

    did the business logic for the user service

--- server-side/intelli-room/src/users/users.service.ts
+++ server-side/intelli-room/src/users/users.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    private users: any = [{id:0, name:'Charbel'}];
+
+    findAll(){
+        return this.users;
+    }
+
+    findById(userId: number){
+        return this.users.find(user => user.id === userId );
+    }
+}
