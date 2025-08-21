418ffacd96e8112d0d50c95624111e5e44f5309d
hibaanadani <hibaanadani1@gmail.com>
2025-08-21 21:45:32 +0300

    fixed the users controller to extend the logic from the service for SOC

--- server-side/intelli-room/src/users/users.controller.ts
+++ server-side/intelli-room/src/users/users.controller.ts
@@ -4,20 +4,18 @@ import { UsersService } from './users.service';
 @Controller('users')
 export class UsersController {
     // nest js utilizing dependancy injection and instantiate classes and manage referenc to those classes
-    constructor(private userService: UsersService){}
+    constructor(private usersService: UsersService){}
 
     @Get()
     getUser(): any{
-        return [{id:0}];
+        return this.usersService.findAll();
     }
 
     // : marks the id as dynamic value URL parameter
     // parse the id from the URL and provide it to the service
     // param 1 to many, specify id and giv eit name id and type string
     @Get(':id')
-    getUserById(@Param('id') id: string): any{
-        return {
-            id
-        }
+    getUserById(@Param('id') id: string): any{ // Nest already parses to int, but we will see it later on
+        return this.usersService.findById(Number(id));
     }
 }
