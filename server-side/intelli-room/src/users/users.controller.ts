3a9fd7e53936afa711b3d6ccd40724b46fa274d8
hibaanadani <hibaanadani1@gmail.com>
2025-08-24 00:33:39 +0300

    added query find by name to the users controller and services

--- server-side/intelli-room/src/users/users.controller.ts
+++ server-side/intelli-room/src/users/users.controller.ts
@@ -1,9 +1,9 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { time } from 'console';
 import { createUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
-import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiOkResponse, ApiQuery, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Users')
 @Controller('users')
@@ -17,10 +17,29 @@ export class UsersController {
         return this.usersService.findAll();
     }
 
+    // get user by querying over, search by id, name...
+    // you can have multipe query params, so here we will specify
+    // ApiQuery makes entering the name non optional
+    @ApiOkResponse({type:User, isArray: true, description: 'get user by Name'})
+    @ApiQuery({name:'name',required: true})
+    @Get('name')
+    getUserByName(@Query('name') name: string): User[]{
+        return this.usersService.findByName(name);
+        // console.log('=== Controller Debug ===');
+        // console.log('Received name parameter:', `"${name}"`);
+        // console.log('Name length:', name.length);
+        
+        // const result = this.usersService.findByName(name);
+        // console.log('Final result:', result);
+        // console.log('Result length:', result.length);
+        
+        // return result;
+    }
+
     // : marks the id as dynamic value URL parameter
     // parse the id from the URL and provide it to the service
     // param 1 to many, specify id and giv eit name id and type string
-    @ApiOkResponse({type:User, isArray: false})
+    @ApiOkResponse({type:User, description: 'get user by ID'})
     @Get(':id')
     getUserById(@Param('id') id: string): User | undefined{ // Nest already parses to int, but we will see it later on
         return this.usersService.findById(Number(id));
--- server-side/intelli-room/src/users/users.service.ts
+++ server-side/intelli-room/src/users/users.service.ts
@@ -5,7 +5,7 @@ import { User } from './entities/user.entity';
 @Injectable()
 export class UsersService {
 
-    private users: User[] = [{id:0, name:'Charbel'}];
+    private users: User[] = [{id:0, name:'Charbel'},{id:1, name:'Taha'},{id:2, name:'Nour'}];
 
     // : User[] provides return type, important to help catch bugs early
     findAll(): User[]{
@@ -13,7 +13,26 @@ export class UsersService {
     }
 
     findById(userId: number): User | undefined{
-        return this.users.find((user) => user.id === userId );
+        return this.users.find(user => user.id === userId );
+    }
+
+    findByName(userName : string ): User[]{
+        return this.users.filter(user => user.name === userName );
+        // console.log('=== Service Debug ===');
+        // console.log('Searching for userName:', `"${userName}"`);
+        // console.log('Available users:');
+        // this.users.forEach((user, index) => {
+        //     console.log(`  [${index}] id: ${user.id}, name: "${user.name}" (length: ${user.name.length})`);
+        // });
+        
+        // const result = this.users.filter(user => {
+        //     const match = user.name === userName;
+        //     console.log(`Comparing "${user.name}" === "${userName}": ${match}`);
+        //     return match;
+        // });
+        
+        // console.log('Filter result:', result);
+        // return result;
     }
 // ... spreading since we will connect to a db lateron and we will need more than the name
     createUser(createUserDto: createUserDto): User{
