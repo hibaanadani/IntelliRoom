14ee8d0413f5abadd5581c11857a55fbc3a5a042
hibaanadani <hibaanadani1@gmail.com>
2025-08-24 23:28:14 +0300

    added passport model and localstrategy to the auth module

--- server-side/intelli-room/src/auth/auth.module.ts
+++ server-side/intelli-room/src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './local.startegy';
 
 @Module({
-    imports:[UsersModule],
-    providers:[AuthService]
+    imports:[UsersModule, PassportModule],
+    providers:[AuthService, LocalStrategy ]
 })
 export class AuthModule {}
