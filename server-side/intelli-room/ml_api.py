87fdd1b722877eed376597a1088e12e307d9933b
hibaanadani <hibaanadani1@gmail.com>
2025-09-21 02:28:51 +0300

    INT-24:change to open icon

--- server-side/intelli-room/ml_api.py
+++ server-side/intelli-room/ml_api.py
@@ -632,4 +632,4 @@ async def get_supported_room_types():
 # Run the application
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
